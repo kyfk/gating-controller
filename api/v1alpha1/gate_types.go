cf50e002d2a8ebeb10e87421a691935ee9582fed
Koya Fukushi <fukushi098@gmail.com>
2022-10-06 01:07:57 +0900

    wip

--- api/v1alpha1/gate_types.go
+++ api/v1alpha1/gate_types.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022.
+Copyright 2022 The Flux authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,29 +17,88 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"time"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+const (
+	SpecDefaultOpened string = "opened"
+	SpecDefaultClosed string = "closed"
+)
+
+const (
+	OpenedCondition string = "Opened"
+)
+
+const (
+	ReconciliationSucceededReason string = "ReconciliationSucceeded"
+	GateClosedReason              string = "GateClosed"
+)
 
 // GateSpec defines the desired state of Gate
 type GateSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Interval at which to check the Gate for updates.
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:Pattern="^([0-9]+(\\.[0-9]+)?(ms|s|m|h))+$"
+	// +required
+	Interval metav1.Duration `json:"interval"`
 
-	// Foo is an example field of Gate. Edit gate_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Default specifies the status of the Gate opened or closed.
+	// Defaults to 'closed', valid values are ('opened', 'closed').
+	// +kubebuilder:validation:Enum=opened;closed
+	// +required
+	Default string `json:"default"`
+
+	// Window specifies the window automatically closes from
+	// the time the Gate has been opened.
+	// +kubebuilder:validation:Pattern="^([0-9]+(\\.[0-9]+)?(ms|s|m|h))+$"
+	// +required
+	Window string `json:"window"`
 }
 
 // GateStatus defines the observed state of Gate
 type GateStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// RequestedAt specifies to close or open the gate ahead of its schedule.
+	// +kubebuilder:validation:Format=date-time
+	// +optional
+	RequestedAt string `json:"requestedAt,omitempty"`
+
+	// ResetToDefaultAt specifies to reset the status of the gate ahead of its schedule.
+	// +kubebuilder:validation:Format=date-time
+	// +optional
+	ResetToDefaultAt string `json:"ResetToDefaultAt,omitempty"`
+
+	// Conditions holds the conditions for the GitRepository.
+	// +optional
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
+// GetConditions returns the status conditions of the object.
+func (in Gate) GetConditions() []metav1.Condition {
+	return in.Status.Conditions
+}
+
+// SetConditions sets the status conditions on the object.
+func (in *Gate) SetConditions(conditions []metav1.Condition) {
+	in.Status.Conditions = conditions
+}
+
+// GetRequeueAfter returns the duration after which the Gate must be
+// reconciled again.
+func (in Gate) GetRequeueAfter() time.Duration {
+	return in.Spec.Interval.Duration
+}
+
+//+genclient
+//+genclient:Namespaced
+//+kubebuilder:storageversion
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:resource:shortName=gt
+//+kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description=""
+//+kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].status",description=""
+//+kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].message",description=""
 
 // Gate is the Schema for the gates API
 type Gate struct {
--- controllers/gate_controller.go
+++ controllers/gate_controller.go
@@ -18,13 +18,17 @@ package controllers
 
 import (
 	"context"
+	"time"
 
+	"github.com/fluxcd/pkg/runtime/conditions"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/fluxcd/gating-controller/api/v1alpha1"
 	gatingv1alpha1 "github.com/fluxcd/gating-controller/api/v1alpha1"
+	"github.com/fluxcd/gating-controller/internal/object"
 )
 
 // GateReconciler reconciles a Gate object
@@ -32,6 +36,11 @@ type GateReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
+
+const (
+	OpenRequestedAnnotation  string = "open.gate.fluxcd.io/requestedAt"
+	CloseRequestedAnnotation string = "close.gate.fluxcd.io/requestedAt"
+)
 
 //+kubebuilder:rbac:groups=gating.toolkit.fluxcd.io,resources=gates,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=gating.toolkit.fluxcd.io,resources=gates/status,verbs=get;update;patch
@@ -47,11 +56,111 @@ type GateReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *GateReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	// log := ctrl.LoggerFrom(ctx)
 
-	// TODO(user): your logic here
+	var gate *gatingv1alpha1.Gate
+	err := r.Get(ctx, req.NamespacedName, gate)
+	if err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	return ctrl.Result{}, nil
+	// Set a condition if the Gate is just created, conditions is nil.
+	if len(gate.Status.Conditions) == 0 {
+		condition := r.defaultOpenedCondition(gate)
+		conditions.Set(gate, condition)
+		return ctrl.Result{RequeueAfter: gate.GetRequeueAfter()}, nil
+	}
+
+	// Patch the status when a user has overridden the annotation.
+	requestedAt, resetToDefaultAt, condition, ok, err := r.newStatusFromAnnotating(gate)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if ok {
+		r.patchStatus(gate, requestedAt, resetToDefaultAt, condition)
+		return ctrl.Result{RequeueAfter: gate.GetRequeueAfter()}, nil
+	}
+
+	err = r.tryResetToDefault(gate)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{RequeueAfter: gate.GetRequeueAfter()}, nil
+}
+
+func (r *GateReconciler) defaultOpenedCondition(gate *v1alpha1.Gate) *metav1.Condition {
+	switch gate.Spec.Default {
+	case v1alpha1.SpecDefaultOpened:
+		return conditions.TrueCondition(v1alpha1.OpenedCondition, v1alpha1.ReconciliationSucceededReason, "Gate scheduled for opening at %s")
+	case v1alpha1.SpecDefaultClosed:
+		return conditions.FalseCondition(v1alpha1.OpenedCondition, v1alpha1.ReconciliationSucceededReason, "Gate closed by default")
+	default:
+		return nil
+	}
+}
+
+func (r *GateReconciler) newStatusFromAnnotating(gate *v1alpha1.Gate) (string, string, *metav1.Condition, bool, error) {
+	annotations := gate.GetAnnotations()
+
+	// Reset the all annotations.
+	defer func() {
+		annotations[OpenRequestedAnnotation] = ""
+		annotations[CloseRequestedAnnotation] = ""
+		gate.SetAnnotations(annotations)
+	}()
+
+	var requestedAtStr string
+	var condition *metav1.Condition
+	if annotations[OpenRequestedAnnotation] != "" {
+		requestedAtStr = annotations[OpenRequestedAnnotation]
+		condition = conditions.TrueCondition(v1alpha1.OpenedCondition, v1alpha1.ReconciliationSucceededReason, "Gate scheduled for closing at %s", requestedAtStr)
+	} else if annotations[CloseRequestedAnnotation] != "" {
+		requestedAtStr = annotations[CloseRequestedAnnotation]
+		condition = conditions.FalseCondition(v1alpha1.OpenedCondition, v1alpha1.ReconciliationSucceededReason, "Gate close requested")
+	} else {
+		return "", "", nil, false, nil
+	}
+
+	requestedAt, err := time.Parse(time.RFC3339, requestedAtStr)
+	if err != nil {
+		return "", "", nil, false, err
+	}
+
+	interval, err := object.GetRequeueInterval(gate)
+	if err != nil {
+		return "", "", nil, false, err
+	}
+
+	resetToDefaultAt := requestedAt.Add(interval)
+
+	return requestedAtStr, resetToDefaultAt.Format(time.RFC3339), condition, true, nil
+}
+
+func (r *GateReconciler) tryResetToDefault(gate *v1alpha1.Gate) error {
+	if gate.Status.ResetToDefaultAt == "" {
+		return nil
+	}
+
+	at, err := time.Parse(time.RFC3339, gate.Status.ResetToDefaultAt)
+	if err != nil {
+		return err
+	}
+
+	// still in the window.
+	if at.After(time.Now()) {
+		return nil
+	}
+
+	// out of the window.
+	r.patchStatus(gate, "", "", r.defaultOpenedCondition(gate))
+	return nil
+}
+
+func (r *GateReconciler) patchStatus(gate *v1alpha1.Gate, requestedAt, resetToDefaultAt string, condition *metav1.Condition) {
+	gate.Status.RequestedAt = requestedAt
+	gate.Status.ResetToDefaultAt = resetToDefaultAt
+	conditions.Set(gate, condition)
 }
 
 // SetupWithManager sets up the controller with the Manager.
